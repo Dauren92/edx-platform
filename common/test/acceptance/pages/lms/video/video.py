c7070c86200ad58c5890ca34849c1989e3ccd361
Ben Patterson <bpatterson@edx.org>
2016-12-11 20:26:56 -0500

    Correct sequencing

--- common/test/acceptance/pages/lms/video/video.py
+++ common/test/acceptance/pages/lms/video/video.py
@@ -636,9 +636,9 @@ class VideoPage(PageObject):
 
         language_selector = VIDEO_MENUS["language"] + ' li[data-lang-code="{code}"]'.format(code=code)
         language_selector = self.get_element_selector(language_selector)
+        self.wait_for_element_visibility(language_selector, 'language menu is visible')
         hover_target = self.q(css=language_selector).results[0]
         ActionChains(self.browser).move_to_element(hover_target).perform()
-        self.wait_for_element_visibility(language_selector, 'language menu is visible')
         self.q(css=language_selector).first.click()
 
         # Sometimes language is not clicked correctly. So, if the current language code
